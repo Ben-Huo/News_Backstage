c8e76afa6330e866405a474bcc49924fdc822183
Ben-Huo <641838789@qq.com>
2019-11-02 20:56:24 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+// 绑定到原型
+Vue.prototype.$axios = axios;
+
+//设置默认的 api 域名
+axios.defaults.baseURL = "http://111.230.181.206:3000";
+
+// 引入 ui 库
+import ElementUI from 'element-ui';
+// 引入 css
+import 'element-ui/lib/theme-chalk/index.css';
+//注册组件库
+Vue.use(ElementUI);
+
 
 Vue.config.productionTip = false
 
