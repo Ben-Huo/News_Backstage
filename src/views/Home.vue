890c1d9bc13223869c4b124b07b9c63b0be623d1
Ben-Huo <641838789@qq.com>
2019-11-04 21:34:39 +0800

    头部头像和昵称

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,60 @@
   <el-container class="wrapper">
     <el-aside width="200px">Aside</el-aside>
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <img class="avatar" v-if="user.head_img" :src="$axios.defaults.baseURL + user.head_img" />
+        <img class="avatar" v-else src="../assets/pic.jpg" alt />
+        {{user.nickname}}
+      </el-header>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      // 从 localStorage 获取用户数据并转换为对象
+      user: JSON.parse(localStorage.getItem("user"))
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
-.wrapper{
+.wrapper {
   position: absolute;
   width: 100%;
   top: 0;
   bottom: 0;
 }
 .el-header {
-  background-color: #b3c0d1;
+  background-color: #58c9b9;
   color: #333;
   text-align: center;
   line-height: 60px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  .avatar{
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    object-fit: cover;
+  margin-right: 10px;
+  }
 }
 
 .el-aside {
-  background-color: #d3dce6;
+  background-color: #519d9e;
   color: #333;
   text-align: center;
   line-height: 260px;
 }
 
 .el-main {
-  background-color: #e9eef3;
+  background-color: #9dc8c8;
   color: #333;
   text-align: center;
   line-height: 160px;
