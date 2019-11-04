3199a9be5d857e6ab364ddf606a99b2f3a86d7e4
Ben-Huo <641838789@qq.com>
2019-11-04 16:53:28 +0800

    实现登录

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,63 @@
 <template>
-  
+  <div>
+    <el-row type="flex" justify="center" align="middle" class="row-bg">
+      <el-form label-width="80px">
+        <el-form-item label="账号">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="form.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="Login">登录</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-row>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    Login() {
+      if (!this.form.username || !this.form.password) {
+        this.$message.error("请输入账号或密码");
+        return;
+      }
+      this.$axios({
+        url: "/login",
+        method: "post",
+        data: this.form
+      }).then(res => {
+        console.log(res.data);
+        if (res.data.message == "登录成功") {
+          localStorage.setItem("token", res.data.data.token);
+          localStorage.setItem("user", JSON.stringify(res.data.data.user));
+          this.$message.success('登陆成功');
+          this.$router.push('/');
+        }
+      });
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.row-bg {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  margin-left: -141px;
+}
 </style>
